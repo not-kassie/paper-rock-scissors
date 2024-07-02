94dcde8766fd02550e3b95b26db4544489d48441
Kassie <kassie.flores99@gmail.com>
2024-07-01 20:14:50 -0500

    added psuedocode to .js and created .html file

---
+++ rockpaperscissors.js
@@ -0,0 +1,45 @@
+//create variables 
+user name: String
+user choice: string 
+user score
+computer score 
+round 
+
+//prompt user for their name make it a pop up??
+print("Enter your name:")
+user name = readLine()!
+
+//pop up shifts to show the three buttons 
+//create three buttons 
+button rock 
+    user choice = rock 
+
+button paper 
+    user choice = paper
+
+button scissors 
+    user choice = scissors
+
+//make computers response, pick randomly, get computer choice function 
+switch (choice){
+    1. rock
+    2. paper
+    3. scissors 
+}
+
+//show users results and computer choice
+prints user: + user choice
+print computer: + computer choice 
+
+playRound(humanChoice, computerChoice) {
+    write you win blank beats blank
+    increment player 
+    write you lose blank beats blank
+    increment computer
+}
+
+while round < 5 {
+    keep prompting and keep score
+} else {
+    game over print results 
+}
